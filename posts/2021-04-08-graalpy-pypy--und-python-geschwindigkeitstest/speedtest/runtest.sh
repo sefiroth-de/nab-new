c2cd202bd5535b86be3475af45e7dfb9673da10e
NMarkgraf <nmarkgraf@hotmail.com>
2025-12-04 09:43:36 +0100

    Update gitirgnore

--- posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
+++ posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-pyenvlist=(pypy3.11-7.3.19 pypy3.11-7.3.19 3.13.5 3.13.10 3.14.0 3.14.1 graalpy-24.1.1 graalpy-community-24.1.1 graalpy-24.2.1 graalpy-community-24.2.1 graalpy-24.2.2 graalpy-community-24.2.2 graalpy-25.0.1 graalpy-community-25.0.1)
+pyenvlist=(pypy3.11-7.3.19 pypy3.11-7.3.20 3.13.5 3.13.10 3.14.0 3.14.1 graalpy-24.1.1 graalpy-community-24.1.1 graalpy-24.2.1 graalpy-community-24.2.1 graalpy-24.2.2 graalpy-community-24.2.2 graalpy-25.0.1 graalpy-community-25.0.1)
+
+echo "" > logfile
 
 for pytn in $pyenvlist; do
   pyenv local $pytn
   echo "******************************************************************"
   echo "Aktuelle Python Version ($pytn):"
   python --version
+  echo -n "- " >> logfile
+  python --version >> logfile
+  echo "" >> logfile
+  python -m timeit -r 50 -n 5 "import nqueens; nqueens.main()" >> logfile
   echo "------------------------------------------------------------------"
-  python -m timeit -r 50 -n 5 "import nqueens; nqueens.main()"
 done;
