2348d095cfcabad50230a80b255d80d92bae248a
NMarkgraf <nmarkgraf@hotmail.com>
2025-12-15 11:09:17 +0100

    Noch ein paar kleinere Updates bzw. Korrekturen.

--- posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
+++ posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
@@ -4,15 +4,14 @@ in="${1:-pyenv-versions.txt}"
 
 [ ! -f "$in" ] && { echo "$0 - File $in not found."; exit 1; }
 
-echo -n "## $(uname -m -n -p -r)" > logfile
-echo -n " am $(date +%Y-%m-%d\ %H:%M:%S)" >> logfile
+echo -n "## Die Ergebnisse des Test vom $(date +%Y-%m-%d\ %H:%M:%S) ($(uname -m -n -p -r))" > logfile
 
 IFS=$'\n' # set the Internal Field Separator to newline
 # printf "%q\n" "$IFS"
 while read -r pytn
 do
     ## avoid commented filename ##
-    [[ $pytn = \#* ]] && echo "\n\n- ${pytn#*\# }" >> logfile && continue
+    [[ $pytn = \#* ]] && echo "\n\n- ${pytn#*\# }:" >> logfile && continue
     pyenv local $pytn
     echo "******************************************************************"
     echo "Aktuelle Python Version ($pytn):"
