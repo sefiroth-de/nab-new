7ff39f905e3e9bca321409bc8466648f8c5090ee
NMarkgraf <nmarkgraf@hotmail.com>
2025-12-15 11:03:41 +0100

    Weiteres Update. Nun produziert das Shell-Skript "runtest.sh" schon fast korrektes Markdown zum Kopieren und Einfügen.

--- posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
+++ posts/2021-04-08-graalpy-pypy--und-python-geschwindigkeitstest/speedtest/runtest.sh
@@ -4,22 +4,22 @@ in="${1:-pyenv-versions.txt}"
 
 [ ! -f "$in" ] && { echo "$0 - File $in not found."; exit 1; }
 
-echo -n "## " > logfile
-uname -m -n -p -r >> logfile
+echo -n "## $(uname -m -n -p -r)" > logfile
+echo -n " am $(date +%Y-%m-%d\ %H:%M:%S)" >> logfile
 
 IFS=$'\n' # set the Internal Field Separator to newline
 # printf "%q\n" "$IFS"
 while read -r pytn
 do
     ## avoid commented filename ##
-    [[ $pytn = \#* ]] && continue
+    [[ $pytn = \#* ]] && echo "\n\n- ${pytn#*\# }" >> logfile && continue
     pyenv local $pytn
     echo "******************************************************************"
     echo "Aktuelle Python Version ($pytn):"
     python --version -V
-    echo -n "- " >> logfile
-    python --version >> logfile
-    python -m timeit -r 50 -n 5 "import nqueens; nqueens.main()" >> logfile
+    echo -n "    - $(python --version -V)" >> logfile
+    echo " \\" >> logfile
+    echo "        $(python -m timeit -r 50 -n 5 'import nqueens; nqueens.main()')" >> logfile
     echo "------------------------------------------------------------------"
 done < "${in}"
 
